17b091f293d250b1470632cb1b9974c598eedcff
shubhamshinde5356 <shindeshubham5356@gmail.com>
2025-07-11 23:49:28 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="">
+      <main className="">
+        <Button variant={"outline"} className="cursor-pointer">
+          click me
+        </Button>
+      </main>
+    </div>
+  );
+}
